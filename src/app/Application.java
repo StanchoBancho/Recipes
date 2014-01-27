55661e65493c16aceae594bdbab362f523556dae
Stanimir Nikolov <nikolov.stanimir@gmail.com>
2014-01-27 16:38:34 +0200

    Basic operations implemented. Now we could add, delete ingredient.

--- src/app/Application.java
+++ src/app/Application.java
@@ -2,29 +2,42 @@ package app;
 
 import java.awt.EventQueue;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.ListSelectionModel;
 
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.factories.FormFactory;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
 import javax.swing.JList;
 import javax.swing.JLabel;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import java.awt.Font;
 
-public class Application {
+public class Application{
 
 	private JFrame frame;
 	private JTextField textField;
+	private DefaultListModel listModel;
 	private JList list;
 	private JLabel lblIngredientList;
+	private JButton btnAddIngredient;
 	private JButton btnSearchForRecipes;
 	private JButton btnEditIngredient;
-	private JButton btnNewButton;
+	private JButton btnDeleteIngredient;
 
 	/**
 	 * Launch the application.
@@ -48,7 +61,91 @@ public class Application {
 	public Application() {
 		initialize();
 	}
+	
+	
+	private void setupTextField(){
+		textField = new JTextField();
+		frame.getContentPane().add(textField, "2, 2, 9, 1, fill, default");
+		textField.setColumns(10);
+		textField.getDocument().addDocumentListener(new DocumentListener() {
+			  public void changedUpdate(DocumentEvent e) {
+				    warn();
+				  }
+				  public void removeUpdate(DocumentEvent e) {
+				    warn();
+				  }
+				  public void insertUpdate(DocumentEvent e) {
+				    warn();
+				  }
 
+				  public void warn() {
+				     if (textField.getText().length()==0){
+							btnAddIngredient.setEnabled(false);
+				     }
+				     else{
+							btnAddIngredient.setEnabled(true);
+				     }
+				  }
+		});
+		textField.addFocusListener(new FocusListener() {
+			@Override
+			public void focusLost(FocusEvent arg0) {
+				// TODO Auto-generated method stub
+			}
+
+			@Override
+			public void focusGained(FocusEvent arg0) {
+				// TODO Auto-generated method stub
+				int[] select = {};
+				list.setSelectedIndices(select);	
+			}
+		});
+	}
+
+	
+	private void setupButtons(){
+		//add button
+		btnAddIngredient = new JButton("Add Ingredient");
+		btnAddIngredient.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String enteredText = textField.getText();
+				listModel.insertElementAt(enteredText, 0);
+				int[] select = {};
+				list.setSelectedIndices(select);
+				textField.setText("");
+			}
+		});
+		btnAddIngredient.setEnabled(false);
+		frame.getContentPane().add(btnAddIngredient, "12, 2");
+	
+		//delete button
+		btnDeleteIngredient = new JButton("Delete Ingredient");
+		frame.getContentPane().add(btnDeleteIngredient, "12, 7");
+		btnDeleteIngredient.setEnabled(false);
+		btnDeleteIngredient.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				int selectedIndex = list.getSelectedIndex();
+				listModel.remove(selectedIndex);
+			}
+		});
+		
+		//edit button
+		btnEditIngredient = new JButton("Edit Ingredient");
+		frame.getContentPane().add(btnEditIngredient, "12, 6");
+		btnEditIngredient.setEnabled(false);
+		btnEditIngredient.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+	}
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -85,32 +182,51 @@ public class Application {
 				FormFactory.DEFAULT_ROWSPEC,
 				FormFactory.RELATED_GAP_ROWSPEC,}));
 		
-		textField = new JTextField();
-		frame.getContentPane().add(textField, "2, 2, 9, 1, fill, default");
-		textField.setColumns(10);
+		setupTextField();
 		
-		JButton btnAddIngredient = new JButton("Add Ingredient");
-		btnAddIngredient.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		listModel = new DefaultListModel();
+		list = new JList(listModel);
+		list.setValueIsAdjusting(true);
+		frame.getContentPane().add(list, "2, 6, 9, 6, fill, fill");
+		list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		list.addListSelectionListener(new ListSelectionListener() {
+			
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+			    if (e.getValueIsAdjusting() == false) {
+
+			        if (list.getSelectedIndex() == -1) {
+			        //No selection, disable fire button.
+			            btnDeleteIngredient.setEnabled(false);
+			            btnEditIngredient.setEnabled(false);
+			        } else {
+			        //Selection, enable the fire button.
+			        	btnDeleteIngredient.setEnabled(true);
+			            btnEditIngredient.setEnabled(true);
+			        }
+			    }
 			}
 		});
-		frame.getContentPane().add(btnAddIngredient, "12, 2");
+		
+		setupButtons();
 		
 		lblIngredientList = new JLabel("Ingredient List");
 		lblIngredientList.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
 		frame.getContentPane().add(lblIngredientList, "2, 4, center, default");
 		
-		list = new JList();
-		frame.getContentPane().add(list, "2, 6, 9, 6, fill, fill");
-		
-		btnEditIngredient = new JButton("Edit Ingredient");
-		frame.getContentPane().add(btnEditIngredient, "12, 6");
-		
-		btnNewButton = new JButton("Delete Ingredient");
-		frame.getContentPane().add(btnNewButton, "12, 7");
 		
 		btnSearchForRecipes = new JButton("Search For Recipes");
 		frame.getContentPane().add(btnSearchForRecipes, "12, 12");
+		btnSearchForRecipes.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				int[] select = {};
+				list.setSelectedIndices(select);
+				
+			}
+		});
 	}
-
 }
