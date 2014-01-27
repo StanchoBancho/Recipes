2d67ab0e73db9b266aee30293948a891e4dbfff9
Stanimir Nikolov <nikolov.stanimir@gmail.com>
2014-01-27 12:41:04 +0200

    Some progress with the UI.

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,10 +1,116 @@
 package app;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import java.awt.Font;
+
 public class Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private JFrame frame;
+	private JTextField textField;
+	private JList list;
+	private JLabel lblIngredientList;
+	private JButton btnSearchForRecipes;
+	private JButton btnEditIngredient;
+	private JButton btnNewButton;
 
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Application window = new Application();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Application() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 702, 450);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(100dlu;default):grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.BUTTON_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.PARAGRAPH_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.PARAGRAPH_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("bottom:default"),
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.PARAGRAPH_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,}));
+		
+		textField = new JTextField();
+		frame.getContentPane().add(textField, "2, 2, 9, 1, fill, default");
+		textField.setColumns(10);
+		
+		JButton btnAddIngredient = new JButton("Add Ingredient");
+		btnAddIngredient.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		frame.getContentPane().add(btnAddIngredient, "12, 2");
+		
+		lblIngredientList = new JLabel("Ingredient List");
+		lblIngredientList.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		frame.getContentPane().add(lblIngredientList, "2, 4, center, default");
+		
+		list = new JList();
+		frame.getContentPane().add(list, "2, 6, 9, 6, fill, fill");
+		
+		btnEditIngredient = new JButton("Edit Ingredient");
+		frame.getContentPane().add(btnEditIngredient, "12, 6");
+		
+		btnNewButton = new JButton("Delete Ingredient");
+		frame.getContentPane().add(btnNewButton, "12, 7");
+		
+		btnSearchForRecipes = new JButton("Search For Recipes");
+		frame.getContentPane().add(btnSearchForRecipes, "12, 12");
 	}
 
 }
