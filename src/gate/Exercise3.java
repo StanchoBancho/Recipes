f35b670314a03d2ad2e0ac636341ccb4ac3650b2
Stanimir Nikolov <nikolov.stanimir@gmail.com>
2014-01-28 18:21:16 +0200

    Some progress with GATE integration

--- src/gate/Exercise3.java
+++ src/gate/Exercise3.java
@@ -2,64 +2,116 @@ package gate;
 
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
 
 import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
 
 import gate.*;
 import gate.creole.ConditionalSerialAnalyserController;
+import gate.creole.ExecutionException;
+import gate.creole.ResourceInstantiationException;
 import gate.gui.*;
+import gate.persist.PersistenceException;
+import gate.util.GateException;
+import gate.util.InvalidOffsetException;
 import gate.util.persistence.PersistenceManager;
 
 //import com.ivita.evtima.utils.Utils;
 
 public class Exercise3 {
-
-	public static void main(String[] args)
-	 throws Exception{
-		
-		System.setProperty("gate.home", "/Applications/gate-7.1-build4485-BIN");
+  
+	public static Corpus getCorpus(){	
+		//System.setProperty("gate.home", "/Applications/gate-7.1-build4485-BIN");
 
 		// prepare the library and clean up the config files 	
-		Gate.init(); 
-		Gate.initConfigData();
+		try {
+			Gate.init();
+			Gate.initConfigData();
+		} catch (GateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} 
 		
 		//make visible
 		MainFrame.getInstance().setVisible(true);
 		
 		//make a corpus
-		Corpus corpus = Factory.newCorpus("docs");
+		Corpus corpus;
+		try {
+			corpus = Factory.newCorpus("docs");
+		} catch (ResourceInstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 
 		//add documents to the corpus;
 		//the documents are in folder "docs" which is in the root folder of the project 
 		File inputDir = new File( "docs/");
 		for(File file : inputDir.listFiles()){
 			// create a new document
-			Document doc = Factory.newDocument(file.toURI().toURL());
-			corpus.add(doc);
+			Document doc = null;
+			try {
+				doc = Factory.newDocument(file.toURI().toURL());
+			} catch (ResourceInstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(doc != null){
+				corpus.add(doc);
+			}
 		}
 		
 		//load an application from a xgapp file which is in the root folder of the project
-		ConditionalSerialAnalyserController myapp = 
-				(ConditionalSerialAnalyserController) 
-		PersistenceManager.loadObjectFromFile
-		(new File("application.xgapp"));
+		ConditionalSerialAnalyserController myapp = null;
+		try {
+			myapp = (ConditionalSerialAnalyserController) PersistenceManager.loadObjectFromFile(new File("app.xgapp"));
+		} catch (PersistenceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (ResourceInstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 		  
 		//set a corpus for the app
 		myapp.setCorpus(corpus);
 		
 		//execute the application
-		myapp.execute();
+		System.out.println("zapochna execute-a");
+		try {
+			myapp.execute();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 		
 		//for each document in the corpus, print the string of each annotation 
 		//of type Person which is in the default annotation set
 		for(Document doc : corpus){
 			for(Annotation ann : doc.getAnnotations().get("Person")){
-				System.out.println(doc.getContent().getContent
-						(ann.getStartNode().getOffset(), 
-								ann.getEndNode().getOffset()));
+				try {
+					System.out.println(doc.getContent().getContent(ann.getStartNode().getOffset(),ann.getEndNode().getOffset()));
+				} catch (InvalidOffsetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
-
-	}
+		System.out.println("svurshvam");
+	return corpus;
+}
 }
